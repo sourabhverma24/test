37b2eb468280e523b080b7673a6614af5f45391d
Sourabh Verma <sourabh24.verma@gmail.com>
2020-08-19 13:32:25 +0530

    crud

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('student','StudentController@index');
+Route::post('student','StudentController@store')->name('student.store');
+Route::get('student/{id}/edit', 'StudentController@edit')->name('student.edit');
+Route::post('student/update', 'StudentController@update')->name('student.update');
+Route::get('student/{id}/delete', 'StudentController@destroy')->name('student.delete');
